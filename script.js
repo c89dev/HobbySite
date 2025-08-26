2e4466806a5e158b11346d145dfd13f423f60a50
c89dev <snarvei@live.com>
2025-08-26 14:50:52 +0200

    layout building

---
+++ script.js
@@ -0,0 +1,19 @@
+function hoverCoding(){
+    document.getElementById("cat1").innerHTML = "Coding";
+    document.getElementById("desc").innerHTML = "My coding journey so far will show some projects I have managed to complete, like a small unity game, a web app and more. ";
+}
+
+function hoverDrawing(){
+    document.getElementById("cat1").innerHTML = "Drawing";
+    document.getElementById("desc").innerHTML = "I have been learning to draw and paint digitally for the last couple years. It is a very deep and difficult skill to master.";
+}
+
+function hoverModeling(){
+    document.getElementById("cat1").innerHTML = "3D Modeling";
+    document.getElementById("desc").innerHTML = "I enjoy the whole process of designing, modeling, texturing and rigging 3D game assets.";
+}
+
+function hoverMusic(){
+    document.getElementById("cat1").innerHTML = "Music";
+    document.getElementById("desc").innerHTML = "For many years I studied guitar, but lately moved more on to electronic music and exploring new genres.";
+}
